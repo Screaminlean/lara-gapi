06ace972619aaf81492964f3d6527b774efd6972
Screaminlean <screaminlean@gmail.com>
2020-02-15 18:22:27 +0000

    Added config.

--- src/Http/Controllers/LaraGapiController.php
+++ src/Http/Controllers/LaraGapiController.php
@@ -2,18 +2,62 @@
 
 namespace Screaminlean\LaraGapi\Http\Controllers;
 
+use Google_Client;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class LaraGapiController extends Controller
 {
-    public function index() {
+    /**
+    * @var \Google_Client
+    */
+    protected $client;
 
-    	return 'Index';
+    /**
+     * Instantiate a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        // create an instance of the google client for OAuth2
+        $this->client = new Google_Client();
+                // set application name
+        $this->client->setApplicationName(Config::get('lara-gapi.application_name'));
+
+        // set oauth2 configs
+        $this->client->setClientId(Config::get('lara-gapi.client_id'));
+        $this->client->setClientSecret(Config::get('lara-gapi.client_secret'));
+        $this->client->setRedirectUri(Config::get('lara-gapi.redirect_uri'));
+        $this->client->setScopes(Config::get('lara-gapi.scopes'));
+        $this->client->setAccessType(Config::get('lara-gapi.access_type'));
+        $this->client->setApprovalPrompt(Config::get('lara-gapi.approval_prompt'));
+
+        // set developer key
+        $this->client->setDeveloperKey(Config::get('lara-gapi.developer_key'));
+
     }
 
-    public function save(Request $request) {
+    /**
+    *
+    * Redirect browser to Google login
+    *
+    * @return      array
+    *
+    */
+    public function redirectTo() {
 
-    	return $request->all(); // just test commit
+        //$google_client->createAuthUrl()
+        return redirect()->away('https://www.google.com');
+    }
+
+    public function handleCallback() {
+
+        return null;
+    }
+
+    public function logout() {
+
+
     }
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,1 +1,9 @@
 <?php
+
+//Namespace for GAPI routes
+Route::namespace('Gapi')->prefix('gapi')->name('gapi.')->group(function () {
+
+    Route::get('/redirect', 'LaraGapiController@redirectTo')->name('redirect');
+    Route::get('/authCallback', 'LaraGapiController@handleCallback')->name('authCallback');
+    Route::get('/logout', 'LaraGapiController@logout')->name('logout');
+});
